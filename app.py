4f006052b49977d944320c1d131378070c82249c
Gabriel Lima Gomes <gabriel.limagomes@br.ey.com>
2023-07-06 15:58:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-#%%writefile app.py
-
 import streamlit as st
-#import folium
-#from streamlit_folium import st_folium
+import folium
+from streamlit_folium import st_folium
 
 st.write('Hello, *World!* :sunglasses:')
 st.title('Eleições de')
 st.title('Aqui estará seu mapa!!! :sunglasses:')
-"""
+
 # center on Liberty Bell, add marker
 m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
 folium.Marker(
@@ -16,4 +14,4 @@ folium.Marker(
 
 # call to render Folium map in Streamlit
 st_data = st_folium(m, width=725)
-"""
+
